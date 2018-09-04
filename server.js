d93e2468c934170ef71167746c8aee128d28dca5
leizhihao <leizhihao@iqianjin.com>
2018-09-04 23:17:34 +0800

    edit port

--- server.js
+++ server.js
@@ -305,7 +305,7 @@ app.post('/api/deleteDir', async function (req, res) {
   }
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(3100, function () {
   var host = server.address().address;
   var port = server.address().port;
 
